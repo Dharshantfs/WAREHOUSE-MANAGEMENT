b9ee3f141a0342a8bee9c7a1e91b9625007f9138
Dharshan S S <dharshan@example.com>
2026-07-16 15:48:54 +0530

    Rename app to warehouse_management to match repository name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 version = "0.0.1"
 
 setup(
-    name="wms_app",
+    name="warehouse_management",
     version=version,
     description="WMS Roll & Bay Stock Tracker",
     author="Antigravity",
